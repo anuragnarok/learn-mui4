e54eb9b0d373c0bf8c11364382432b9219ffcb1e
Anurag <anuragchakravorty000@gmail.com>
2022-10-28 11:24:18 +0530

    second commit

--- src/components/MuiComponent.js
+++ src/components/MuiComponent.js
@@ -1,47 +1,117 @@
-import React from 'react'
-import {Typography,AppBar,Card,CardActions,CardContent,CardMedia,CssBaseline,Grid,Toolbar,Button,Container} from '@material-ui/core';
-import {PhotoCamera} from '@material-ui/icons';
-import useStyles from './styles';
+import React from "react";
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Button,
+  Container,
+} from "@material-ui/core";
+import { PhotoCamera } from "@material-ui/icons";
+import useStyles from "./styles";
+const cards = [1,2,3,4,5,6,7,8,9]
 function MuiComponent() {
-    const classes = useStyles()
+  const classes = useStyles();
   return (
     <div>
-        <CssBaseline/>
-        <AppBar position='relative'>
-            <Toolbar>
-                <PhotoCamera/>
-                <Typography variant='h6'>
-                    Photo Album
-                </Typography>
-            </Toolbar>
-        </AppBar>
-        <main>
-            <div className={classes.container}>
-                <Container maxWidth='sm' >
-                    <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
-                        Photo Album
-                    </Typography>
-                    <Typography variant='h5' align='center' color='textSecondary' paragraph>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat nulla odit vel nobis laboriosam exercitationem nihil ratione reiciendis at? Nihil tempore unde repellendus assumenda rem, animi dolor impedit voluptate numquam.
-                    </Typography>
-                    <div>
-                        <Grid container spacing={2} justify= 'center'>
-                            <Grid item>
-                                <Button variant='contained' color='primary'> See my Photos</Button>
-                            </Grid>
-                            <Grid item>
-                                <Button variant='outlned' color='primary'> Secondary action</Button>
-                            </Grid>
-                            
-
-
-                        </Grid>
-                    </div>
-                </Container>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera className={classes.icon} />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat
+              nulla odit vel nobis laboriosam exercitationem nihil ratione
+              reiciendis at? Nihil tempore unde repellendus assumenda rem, animi
+              dolor impedit voluptate numquam.
+            </Typography>
+            <div className={classes.buttons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    {" "}
+                    See my Photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlned" color="primary">
+                    {" "}
+                    Secondary action
+                  </Button>
+                </Grid>
+              </Grid>
             </div>
-        </main>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+
+            <Grid item key= {card} xs={12} sm={6} md={4}>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image="https://source.unsplash.com/random"
+                  title="Image Title"
+                />
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="h5">
+                    Heading
+                  </Typography>
+                  <Typography>
+                    This is a media card. You can use this section to describe
+                    the content.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    {" "}
+                    view
+                  </Button>
+                  <Button size="small" color="primary">
+                    {" "}
+                    Edit
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+            Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary">
+            Something to give a footer a purpose
+        </Typography>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default MuiComponent
+export default MuiComponent;
--- src/components/styles.js
+++ src/components/styles.js
@@ -1,4 +1,6 @@
 import {makeStyles} from '@material-ui/core';
+// in the above line, we imported a makeStyles. makeStyles is a function from Material-UI that allows us to create CSS classes and rules using JavaScript objects
+// Below, we created a hook useStyles which will take the classnames of various components as a prop and execute CSS styles on it.
 const useStyles = makeStyles((theme)=> ({
     container: {
         backgroundColor: theme.palette.background.paper,
@@ -9,7 +11,25 @@ const useStyles = makeStyles((theme)=> ({
     },
     buttons: {
         marginTop: '40px',
+    },
+    cardGrid: {
+        padding: '20px 0'
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    cardMedia: {
+        paddingTop: '56.25%'
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: '50px 0'
     }
 }))
-
+// we need to export the hook so that we can import it in other files.
 export default useStyles;
